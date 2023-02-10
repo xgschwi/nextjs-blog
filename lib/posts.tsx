35bb98e374896bdbc5b53acb92e6e8f6b5b3722a
Xavier Gschwind <xaviergschwind@gmail.com>
2023-02-09 20:14:17 -0500

    Typescript Conversion

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as {date: string; title: string}),
     };
   });
   // Sort posts by date
@@ -63,6 +63,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as {date: string; title: string})
   }
 }
